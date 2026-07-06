45495b6bf3dffd26c3fdf8763f01db2ffd827eef
Nic Pereira <github@nicpereira.dev>
2026-07-06 14:10:08 -0500

    Star bounce

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 import './Header.css'
 
 export default function Header() {
   return (<div className="header">
-    <h1><span className="highlight">KAR</span> Router!</h1>
+    <h1>
+      <span className="star-wrap">
+        <FontAwesomeIcon className="star" icon={faStar} />
+        <FontAwesomeIcon className="spark spark-1" icon={faStar} />
+        <FontAwesomeIcon className="spark spark-2" icon={faStar} />
+        <FontAwesomeIcon className="spark spark-3" icon={faStar} />
+      </span>
+      <span className="highlight">KAR</span>
+      <span className="base-text">Router!</span>
+    </h1>
   </div>)
 };
